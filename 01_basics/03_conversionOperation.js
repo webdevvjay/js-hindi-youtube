3b254cc5c4f27b02d2e018e22bfd06e784b3a517
Vijay Singh Diggi <105920990+webdevvjay@users.noreply.github.com>
2024-03-03 20:05:31 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "vijay"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false=> 0
+
+let isLoggedIn = "vijay";
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "vijay" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber)
+console.log(typeof stringNumber);
