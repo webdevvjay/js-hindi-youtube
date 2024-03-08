91ea08a2ecc2034f2cc3b512434ff814aa550527
Vijay Singh Diggi <105920990+webdevvjay@users.noreply.github.com>
2024-03-08 18:10:38 +0000

    data and time

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,4 +1,4 @@
-// Dates
+0// Dates
 
 let myDate = new Date()
 // console.log(myDate);
@@ -20,4 +20,16 @@ let myTimeStamp = Date.now()
 
 // console.log(myTimeStamp)
 // console.log(myCreatedDate.getTime())
-// console.log(Math.floor(Date.now()/1000))
+//console.log(Math.floor(Date.now()/1000))
+
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth());
+console.log(newDate.getDay())
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    timeZone: ''
+})
+
