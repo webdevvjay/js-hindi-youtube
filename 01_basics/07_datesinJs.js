a6f576bb936b3e8ab2ed1f56e4065d4997afc4e9
Vijay Singh Diggi <105920990+webdevvjay@users.noreply.github.com>
2024-03-08 14:44:31 +0000

    dates and time

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,23 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toUTCString())
+// console.log(myDate.toDateString())
+// console.log(typeof myDate)
+
+// let myCreatedDate = new Date(2023, 0, 23)
+
+//let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
