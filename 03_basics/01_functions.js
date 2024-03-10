3ee11220dc20f99c04072e80cc18895fd6f2037e
Vijay Singh Diggi <105920990+webdevvjay@users.noreply.github.com>
2024-03-10 13:51:31 +0000

    functions with objects

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -31,9 +31,43 @@ function loginUserMessage(username = "Sam") {
         return
 
     }
-    return `${username} just logged in`
+    //return `${username} just logged in`
 }
 
 loginUserMessage("Vijay") // this function is executed but we have not instructed it to print it.
 
-console.log(loginUserMessage("Vijay"))
+//console.log(loginUserMessage("Vijay"))
+
+// function with objects
+
+function calculateCartPrice(val1, val2,...num1) {
+    return num1
+}
+// ... is also called rest operator
+
+//console.log(calculateCartPrice(200, 400, 500, 600, 700))
+
+const user = {
+    username:"Vijay",
+    prices: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+//handleObject(user)
+
+handleObject({
+    username: "Sam Altman",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray) {
+    return getArray[2]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 500, 300, 400, 800]))
