6ff14e1777cf1d803f690f9534cbd95f90c66077
Vijay Singh Diggi <105920990+webdevvjay@users.noreply.github.com>
2024-03-11 12:22:00 +0000

    for while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+// let i = 0
+// while (i <= 10) {
+//     console.log(`Value of index is ${i}`)
+//     i = i + 2
+// }
+
+// let myArray = ["flash", "batman", "superman"]
+// let j= 0
+// while(j<myArray.length) {
+//  console.log(`Value is ${myArray[j]}`) 
+//  j++ 
+// }
+
+let score = 11
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score <= 10);
