f32c52ffdae96cbb2ef8c750ab1b59c0704dc438
Vijay Singh Diggi <105920990+webdevvjay@users.noreply.github.com>
2024-03-13 15:24:28 +0000

    forEach, forOf, forIn

---
+++ 05_iterations/four.js
@@ -0,0 +1,30 @@
+const myObject = {
+  js: "javascript",
+  cpp: "c++",
+  rb: "ruby",
+  swift: "swift by apple",
+};
+
+// for (const key in myObject) {
+//   console.log(`${key} shorcut is for ${myObject[key]}`);
+// }
+
+// const programming = ['js', 'rb', 'py', 'java', 'cpp'] 
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+
+
+// const map = new Map()
+
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+
+// for (const key in map) {
+//    console.log(key)
+// }
+
+
+
--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -27,4 +27,18 @@ map.set('IN', "India")
 map.set('USA', "United States of America")
 map.set('Fr', "France")
 
-console.log(map)
+//console.log(map)
+
+// for (const [key, value] of map) {
+//     console.log(key, ":-", value)
+// }
+
+const myObject = {
+    'game1': "NFS",
+    'game2': "Spiderman"
+}
+
+// for  (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
